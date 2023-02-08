7e6975307d8fc0f20bf8d7cc4f6bd17ea17f75c1
pjstrauss12 <parkerstrauss21@gmail.com>
2023-02-08 14:09:04 -0500

    Gonna revisit this later to figure out why implementing the JS breaks the card

--- src/App.vue
+++ src/App.vue
@@ -5,67 +5,15 @@
   <p>Opening in 2018 at Cedar Point in Sandusky, Ohio, Steel Vengeance has quickly became a fan favorite.  Rocky Mountain Construction, a company that retracks old wooden roller coasters, came in to revamp Mean Streak and made it a hybrid roller coaster.  This means it has a wood structure, but the trains run along a steel track.  This allows for a better experience overall and increases the lifespan of older wooden roller coasters.  The ride is 205 feet tall with a 200 foot drop at 90 degrees and it flips you upside down four times.  The top speed of Steel Vengeance is 74 miles per hour and the total ride time is 2 minutes and 30 seconds.  Learn more about the ride by clicking below.</p>
     <button class="details">Details</button>
 </div>
-<button class="add" v-on:click="duplicator">Make a duplicate</button>
+<button class="add">Make a duplicate</button>
 
-<button class="toggle" v-on:click="backColorChange">Background Color</button>
+<button class="toggle">Background Color</button>
 
-<button class="word" v-on:click="wordChanger">Words</button>
+<button class="word">Words</button>
 
-<button class="del" v-on:click="deleter">Remove Last</button>
+<button class="del">Remove Last</button>
 </template>
 
-<script>
-
-let btn = document.querySelector('.add')
-btn.addEventListener('mouseover', () => btn.style.backgroundColor= 'green'); 
-btn.addEventListener('mouseover', () => btn.style.color= 'yellow'); 
-btn.addEventListener('mouseout', () => btn.style.backgroundColor= 'red');
-btn.addEventListener('mouseout', () => btn.style.color= 'white');
-export default{
-methods:{
- duplicator(){
-  let d = document.querySelector('div');
-  let cloneNode = d.cloneNode(true);
-  document.body.appendChild(cloneNode);
-  console.log(cloneNode);
-},
-
- backColorChange(){
-  var colors = document.querySelector(".fullcard");
-  if (colors.style.backgroundColor === "black"){
-    colors.style.backgroundColor = "blue";
-  }else{
-    colors.style.backgroundColor = "black";
-  }
-},
-
- wordChanger(){
-  let thingy = document.querySelector('.word').forEach(thing);
-  if(!thingy.innerHTML == "Something else"){
-    thingy.innerHTML = "Something else";
-  }
-},
-
-
- deleter(){
-  let d = document.querySelectorAll('div');
-  var last = d[d.length- 1];
-  if (d.length != 1){
-  last.remove();
-  }
-},
-
- showhide(){
-  var para = document.querySelector("p");
-  if (para.style.display === "none"){
-    para.style.display = "inline-block";
-  }else{
-    para.style.display = "none";
-  }
-}
-}
-}
-</script>
 <style>
 .fullcard {
   border: 5px solid;
